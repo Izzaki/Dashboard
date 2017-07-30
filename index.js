1549664454ed5a910ad4fcb06c8fdb9eadd4e808
Robert <banany@g.pl>
2017-07-30 16:44:42 +0200

    first prototype of HomerDashboard

---
+++ index.js
@@ -0,0 +1,11 @@
+HomerDashboard = (function(){
+	
+	// constructor
+	return function(HomerDashboardData){
+		this.class = 'homer-dashboard'; // name of class HomerDashboard should search for
+		
+		this.show(){
+			
+		};
+	}
+})();
