aa025d2bde6da3908bbf0d8a4c5a2961b1598dfb
Robert <banany@g.pl>
2017-07-30 16:51:26 +0200

    fix prototypes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 HomerDashboard = (function(){
 	
 	// constructor
-	return function(HomerDashboardData){
+	return function(homerDashboardData){
 		this.class = 'homer-dashboard'; // name of class HomerDashboard should search for
 		
-		this.show(){
+		this.show = function(){
 			
-		};
+		}
 	}
 })();
 
 HomerDashboardData = (function(){
 	
 	return function(dataTarget){
-		
+		this.name = dataTarget;
 	}
 })();
