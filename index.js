cb49b7a8606a2c96d38a766a81eb86411d637ee4
Robert <banany@g.pl>
2017-07-30 19:11:07 +0200

    added entity property | added exceptions

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ HomerDashboard = (function(){
 	// constructor
 	return function(homerDashboardData){
 		this.class = 'homer-dashboard'; // name of class HomerDashboard should search for
+		this.entity = document.querySelector(`.${this.class}[data-target="${homerDashboardData.name}"]`); 
 		
 		this.show = function(){
-			
+			try{
+				if(!this.entity) throw 'entity not found';
+			}catch(e){
+				console.log('Cannot show dashboard caused by: ', e);
+			}
 		}
 	}
 })();
